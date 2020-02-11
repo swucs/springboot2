d033127199907e171161c2ab3779a782fbb36748
swucs <swucs7@gmail.com>
2020-02-11 17:49:33 +0900

    test

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@EnableJpaAuditing
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
